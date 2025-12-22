1e63dfb183a7baf838cf1becd55541da1c99599a
ratul-hasan00 <afif.ratul00@gmail.com>
2025-12-22 23:48:33 +0600

    added

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({ origin: [process.env.SITE_DOMAIN], credentials: true }));
+app.use(cors({
+    origin: process.env.SITE_DOMAIN,
+    credentials: true
+}));
+
 app.use(express.json());
 
 // MongoDB
